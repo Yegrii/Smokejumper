693f762199e19526c1d77aedce7bff447f0f9ee2
Yegor <viryutinyegor@gmail.com>
2023-05-29 17:49:00 +0300

    test

--- Smokejumper/main.py
+++ Smokejumper/main.py
@@ -1,78 +1,16 @@
 import fastf1
-import fastf1.plotting
-import seaborn as sns
 from matplotlib import pyplot as plt
+import fastf1.plotting
+cache_to_dir = '/Users/anastasiakuzmenko/Desktop/cachesF1/'
+fastf1.Cache.enable_cache(cache_to_dir)
+session = fastf1.get_session(2023, 3, 'R')
+print(session.date)
+print(session.name)
+print(session.event)
 
-# enabling misc_mpl_mods will turn on minor grid lines that clutters the plot
-fastf1.plotting.setup_mpl(mpl_timedelta_support=False, misc_mpl_mods=False)
+schedule = fastf1.get_event_schedule(2023)
+print(schedule)
 
-# Load the race session
-race = fastf1.get_session(2023, "Azerbaijan", 'R')
-race.load()
-
-# Get all the laps for the point finishers only. Filter out slow laps (yellow flag, VSC, pitstops etc.)
-# as they distort the graph axis.
-point_finishers = race.drivers[:10]
-print(point_finishers)
-driver_laps = race.laps.pick_drivers(point_finishers).pick_quicklaps()
-driver_laps = driver_laps.reset_index()
-# Out: ['11', '1', '16', '14', '55', '44', '18', '63', '4', '22']
-
-
-# To plot the drivers by finishing order, we need to get their three-letter abbreviations in the finishing order.
-finishing_order = [race.get_driver(i)["Abbreviation"] for i in point_finishers]
-print(finishing_order)
-# Out: ['PER', 'VER', 'LEC', 'ALO', 'SAI', 'HAM', 'STR', 'RUS', 'NOR', 'TSU']
-
-
-# We need to modify the DRIVER_COLORS palette.
-# Its keys are the driver’s full names but we need the keys to be the drivers’ three-letter abbreviations.
-# We can do this with the DRIVER_TRANSLATE mapping.
-driver_colors = {abv: fastf1.plotting.DRIVER_COLORS[driver] for abv,
-                 driver in fastf1.plotting.DRIVER_TRANSLATE.items()}
-print(driver_colors)
-# Out: {'LEC': '#dc0000', 'SAI': '#ff8181', 'VER': '#0600ef', 'PER': '#716de2', 'PIA': '#ff8700', 'NOR': '#eeb370',
-# 'GAS': '#0090ff', 'OCO': '#70c2ff', 'BOT': '#900000', 'ZHO': '#500000', 'DEV': '#2b4562', 'TSU': '#356cac',
-# 'MAG': '#ffffff', 'HUL': '#cacaca', 'ALO': '#006f62', 'STR': '#25a617',
-# 'DRU': '#2f9b90', 'HAM': '#00d2be', 'RUS': '#24ffff', 'ALB': '#005aff', 'SAR': '#012564'}
-
-# First create the violin plots to show the distributions. Then use the swarm plot to show the actual laptimes.
-
-# create the figure
-fig, ax = plt.subplots(figsize=(10, 5))
-
-# Seaborn doesn't have proper timedelta support
-# so we have to convert timedelta to float (in seconds)
-driver_laps["LapTime(s)"] = driver_laps["LapTime"].dt.total_seconds()
-
-sns.violinplot(data=driver_laps,
-               x="Driver",
-               y="LapTime(s)",
-               inner=None,
-               scale="area",
-               order=finishing_order,
-               palette=driver_colors
-               )
-
-sns.swarmplot(data=driver_laps,
-              x="Driver",
-              y="LapTime(s)",
-              order=finishing_order,
-              hue="Compound",
-              palette=fastf1.plotting.COMPOUND_COLORS,
-              hue_order=["SOFT", "MEDIUM", "HARD"],
-              linewidth=0,
-              size=5,
-              )
-
-# Out: <Axes: xlabel='Driver', ylabel='LapTime(s)'>
-
-# Make the plot more aesthetic
-
-ax.set_xlabel("Driver")
-ax.set_ylabel("Lap Time (s)")
-plt.suptitle("2023 Azerbaijan Grand Prix Lap Time Distributions")
-sns.despine(left=True, bottom=True)
-
-plt.tight_layout()
-plt.show()
+session.load()
+print(session.results)
+print(session)
