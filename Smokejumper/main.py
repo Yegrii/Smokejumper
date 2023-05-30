5aab80bddf1ce20825b3d027b7ed4aed8dbef387
Yegor <viryutinyegor@gmail.com>
2023-05-30 11:33:35 +0300

    Familiarization with the documentation

--- Smokejumper/main.py
+++ Smokejumper/main.py
@@ -3,14 +3,65 @@ from matplotlib import pyplot as plt
 import fastf1.plotting
 cache_to_dir = '/Users/anastasiakuzmenko/Desktop/cachesF1/'
 fastf1.Cache.enable_cache(cache_to_dir)
-session = fastf1.get_session(2023, 3, 'R')
-print(session.date)
-print(session.name)
-print(session.event)
-
-schedule = fastf1.get_event_schedule(2023)
-print(schedule)
-
+session = fastf1.get_session(2023, 5, 'R')
 session.load()
+schedule = fastf1.get_event_schedule(2023)
+laps = session.laps
+
+
+print(session.date)
+# print(session.name)
+# print(session.event)
+print(schedule)
 print(session.results)
 print(session)
+print(laps)
+
+
+drivers = session.drivers
+print(drivers)
+
+drivers = [session.get_driver(driver)["Abbreviation"] for driver in drivers]
+print(drivers)
+
+stints = laps[["Driver", "Stint", "Compound", "LapNumber"]]
+stints = stints.groupby(["Driver", "Stint", "Compound"])
+stints = stints.count().reset_index()
+
+stints = stints.rename(columns={"LapNumber": "StintLength"})
+print(stints)
+
+
+fig, ax = plt.subplots(figsize=(5, 10))
+
+for driver in drivers:
+    driver_stints = stints.loc[stints["Driver"] == driver]
+
+    previous_stint_end = 0
+    for idx, row in driver_stints.iterrows():
+        # each row contains the compound name and stint length
+        # we can use these information to draw horizontal bars
+        plt.barh(
+            y=driver,
+            width=row["StintLength"],
+            left=previous_stint_end,
+            color=fastf1.plotting.COMPOUND_COLORS[row["Compound"]],
+            edgecolor="black",
+            fill=True
+        )
+
+        previous_stint_end += row["StintLength"]
+
+
+plt.title("2023 Monaco Grand Prix Strategies")
+plt.xlabel("Lap Number")
+plt.grid(False)
+# invert the y-axis so drivers that finish higher are closer to the top
+ax.invert_yaxis()
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
+
+plt.tight_layout()
+plt.show()
