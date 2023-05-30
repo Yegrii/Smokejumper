d7c2abfdfa2d8ec2360fe25b3fdb4a81a8865d57
Yegor <viryutinyegor@gmail.com>
2023-05-30 12:26:44 +0300

    extraction best lap time

--- Smokejumper/main.py
+++ Smokejumper/main.py
@@ -3,65 +3,36 @@ from matplotlib import pyplot as plt
 import fastf1.plotting
 cache_to_dir = '/Users/anastasiakuzmenko/Desktop/cachesF1/'
 fastf1.Cache.enable_cache(cache_to_dir)
-session = fastf1.get_session(2023, 5, 'R')
+session = fastf1.get_session(2023, 1, 'R')
 session.load()
 schedule = fastf1.get_event_schedule(2023)
 laps = session.laps
-
-
-print(session.date)
-# print(session.name)
-# print(session.event)
-print(schedule)
-print(session.results)
-print(session)
-print(laps)
-
-
 drivers = session.drivers
-print(drivers)
-
-drivers = [session.get_driver(driver)["Abbreviation"] for driver in drivers]
-print(drivers)
-
-stints = laps[["Driver", "Stint", "Compound", "LapNumber"]]
-stints = stints.groupby(["Driver", "Stint", "Compound"])
-stints = stints.count().reset_index()
-
-stints = stints.rename(columns={"LapNumber": "StintLength"})
-print(stints)
 
 
-fig, ax = plt.subplots(figsize=(5, 10))
-
-for driver in drivers:
-    driver_stints = stints.loc[stints["Driver"] == driver]
-
-    previous_stint_end = 0
-    for idx, row in driver_stints.iterrows():
-        # each row contains the compound name and stint length
-        # we can use these information to draw horizontal bars
-        plt.barh(
-            y=driver,
-            width=row["StintLength"],
-            left=previous_stint_end,
-            color=fastf1.plotting.COMPOUND_COLORS[row["Compound"]],
-            edgecolor="black",
-            fill=True
-        )
-
-        previous_stint_end += row["StintLength"]
+# print(session.date)
+# # print(session.name)
+# # print(session.event)
+# print(schedule)
+# print(session.results)
+# print(session)
+# print(laps)
+# print(drivers)
 
 
-plt.title("2023 Monaco Grand Prix Strategies")
-plt.xlabel("Lap Number")
-plt.grid(False)
-# invert the y-axis so drivers that finish higher are closer to the top
-ax.invert_yaxis()
+best_bottas = session.laps.pick_driver('BOT').pick_fastest()
+print(best_bottas['LapTime'])
+lap_time = best_bottas['LapTime']
 
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
 
-plt.tight_layout()
-plt.show()
+# Функция для форматирования времени
+def strfdelta(tdelta, fmt):
+    d = {"days": tdelta.days}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+    d["milliseconds"] = tdelta.microseconds // 1000
+    return fmt.format(**d)
+
+
+formatted_time = strfdelta(lap_time, "{minutes:02d}:{seconds:02d}.{milliseconds:03d}")
+print(formatted_time)
